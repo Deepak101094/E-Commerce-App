f68b795cecf7a92280bb1689f5b58557f64eae88
deepak pandey <deepakpandey101094@gmail.com>
2020-05-24 17:07:10 +0530

    use react-hook-form for logIn page

--- src/form/Login.js
+++ src/form/Login.js
@@ -1,105 +1,144 @@
-import React, { Component } from "react";
-//? Redux
+import React from 'react';
 import { connect } from "react-redux";
-import { Field, reduxForm } from "redux-form";
-//? Utilities
-import validate from "../utility/validate/index";
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+//? react-hook-form
+import { useForm } from "react-hook-form";
+//? layout
+import layout from "../Hoc/Layout";
 //? action
 import { loginUser } from "../store/actions/login";
-//? Material UI
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import MUCard from "@material-ui/core/Card";
-import { styled } from "@material-ui/styles";
-//? Actions
+//? material-ui
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { FETCH_PRODUCTS_SUCCESS } from '../store/actionTypes';
+import Layout from '../Hoc/Layout';
 
-//? Layout
-import Layout from "../Hoc/Layout";
-
-const Card = styled(MUCard)({
-  padding: "30px",
-  margin: "30px"
-});
-
-class Login extends Component {
-  renderError = ({ touched, error }) => {
-    if (touched && error) {
-      return (
-        <div className="error">
-          <style jsx>{`
-            .error {
-              margin: 10px 0px;
-              color: red;
-            }
-          `}</style>
-          {error}
-        </div>
-      );
-    }
-  };
-
-  renderField = ({ input, label, type, meta }) => {
-    return (
-      <div>
-        <label>{label}</label>
-        <div>
-          <TextField
-            {...input}
-            type={type}
-            placeholder={label}
-            fullWidth={true}
-          />
-          {this.renderError(meta)}
-        </div>
-      </div>
-    );
-  };
-
-  formSubmit = (reqBody) => { 
-   const { loginUser } = this.props;
- // console.log(reqBody); 
-  loginUser(reqBody);
-  };
- 
-  render() {
-    const { handleSubmit } = this.props;
-    return (
-      <div className="container">
-        <Card>
-          <form onSubmit={handleSubmit(this.formSubmit)}>
-            <Field
-              name="email"
-              component={this.renderField}
-              type="email"
-              label="Email"
-            />
-            <Field
-              name="password"
-              component={this.renderField}
-              type="password"
-              label="Password"
-            />
-           
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              size="medium"
-            >
-              Login
-            </Button>
-          </form>
-        </Card>
-      </div>
-    );
-  }
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" to= "/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-Login = connect(null, { loginUser })(Login);
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
-const ProductForm = reduxForm({
-  form: "addProductForm",
-  validate
-})(Login);
+function SignIn(props) {
+  const classes = useStyles();
+  const history = useHistory();
+  const { handleSubmit, errors, reset, register } = useForm();
 
-export default Layout(ProductForm);
+  const reqBodyHandler = (reqBody, e) => {
+   //console.log(reqBody);
+   const { loginUser } = props;
+   loginUser(reqBody);
+   e.target.reset();
+   history.push("/")
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} onSubmit= {handleSubmit(reqBodyHandler)} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            inputRef={register({ required: true })}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            inputRef={register({ required: true })}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link to="/signup" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
+ 
+const login = connect(null, { loginUser })(SignIn)
+
+export default Layout(login);
