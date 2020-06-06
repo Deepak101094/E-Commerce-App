39e0c3134641091485ad89a5fc48f4d4e2f8d0c0
deepak pandey <deepakpandey101094@gmail.com>
2020-06-06 18:41:15 +0530

    used react hook form in update-product form

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ import "./index.css";
 //? Components
 import Home from "./Home";
 import AddProduct from "../src/containers/AddProduct";
-import UpdateProduct from "../src/containers/UpdateProduct";
+import ProductList from "./containers/ProductList";
+import UpdateProduct from "./containers/UpdateProduct";
 import Login from "../src/form/Login";
 import SignUp from "../src/form/SignUp";
 import CartItems from "../src/containers/Cart-Items";
@@ -21,43 +22,49 @@ import Orders from "../src/containers/orders";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const persistConfig = {
-   key: "root",
-   storage,
-   //  blacklist: [],
-   //  whitelist: ["addProduct"]
+  key: "root",
+  storage,
+  //  blacklist: [],
+  //  whitelist: ["addProduct"]
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-export const store = createStore(persistedReducer, composeEnhancers(applyMiddleware(thunk)));
+export const store = createStore(
+  persistedReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 persistStore(store);
 
 ReactDOM.render(
-   <Provider store={store}>
-      <Router>
-         <Switch>
-            <Route exact path="/orders">
-               <Orders />
-            </Route>
-            <Route exact path="/">
-               <Home />
-            </Route>
-            <Route path="/signup">
-               <SignUp />
-            </Route>
-            <Route path="/login">
-               <Login />
-            </Route>
-            <Route path="/add-product">
-               <AddProduct />
-            </Route>
-            <Route path="/update-product/:id">
-               <UpdateProduct />
-            </Route>
-            <Route path="/cart-item">
-               <CartItems />
-            </Route>
-         </Switch>
-      </Router>
-   </Provider>,
-   document.getElementById("root")
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route exact path="/orders">
+          <Orders />
+        </Route>
+        <Route exact path="/product-list">
+          <ProductList />
+        </Route>
+        <Route exact path="/add-product">
+          <AddProduct />
+        </Route>
+        <Route exact path="/update-product:id">
+          <UpdateProduct />
+        </Route>
+        <Route exact path="/cart-item">
+          <CartItems />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/signup">
+          <SignUp />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  </Provider>,
+  document.getElementById("root")
 );
--- src/store/actions/update-product.js
+++ src/store/actions/update-product.js
@@ -3,7 +3,7 @@ import axios from "../../utility/axios/withHeader";
 import _get from "lodash/get";
 import _isEmpty from "lodash/isEmpty";
 
-export const updateProduct = (reqBody) => {
+export const updateProduct = (reqBody, cbfunc) => {
    return async (dispatch) => {
       dispatch({
          type: UPDATE_PRODUCT_INIT,
@@ -27,6 +27,7 @@ export const updateProduct = (reqBody) => {
                error: "",
             },
          });
+         cbfunc();
       } catch (err) {
          const error = _get(err, "response.data.message", "some error occurred!");
          dispatch({
