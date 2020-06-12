3eba641d3f92594844528d5f1d89a10e5d5bf1e6
Shubham Chitransh <shubhamchitransh8@gmail.com>
2020-06-12 13:30:47 +0530

    refreshing table after delete product

--- src/containers/ProductList.js
+++ src/containers/ProductList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { useHistory } from "react-router-dom";
 //? action
@@ -10,84 +10,91 @@ import axios from "../utility/axios/withHeader";
 import _get from "lodash/get";
 //? import from  material
 import MaterialTable from "material-table";
+import Loader from "@material-ui/core/CircularProgress";
 import { Grid } from "@material-ui/core";
-
 
 const ProductList = (props) => {
    const history = useHistory();
-  const deleteProductHandler = (productId) => {
-      const { deleteProduct } = props;
-      deleteProduct(productId);
-   };
+   const tableRef = React.createRef();
 
    const editProductHandler = (productId) => {
-    const { fetchSingleProduct }= props;
-    fetchSingleProduct(productId);
-    history.push(`/update-product${productId}`)
-   }
+      const { fetchSingleProduct } = props;
+      fetchSingleProduct(productId);
+      history.push(`/update-product${productId}`);
+   };
 
-  const tableColumns = [
+   const tableColumns = [
       { title: "Name", field: "name" },
       { title: "Price", field: "price" },
       { title: "Description", field: "description" },
    ];
-   
-      return (
+
+   return (
+      <div className="container">
          <Grid container>
-         <Grid item xs={12}> 
-            {
-               <MaterialTable
-                  title="Products List"
-                  options={{
-                     search: false,
-                     pageSize: 10,
-                     pageSizeOptions: [5, 10, 15, 20, 25, 30],
-                  }}
-                  columns={tableColumns}
-                  actions={[
-                     {
-                        icon: "edit",
-                        tooltip: "edit product",
-                        onClick: (event, rowData) => {
-                           // Do save operation
-                           //  console.log(rowData)
-                           editProductHandler(rowData._id)
-                        },
-                     },
-                     {
-                        icon: "delete",
-                        tooltip: "delet product",
-                        onClick: (event, rowData) => {
-                           // Do save operation
-                           //  console.log(rowData)
-                           deleteProductHandler(rowData._id);
-                        },
-                     },
-                  ]}
-                  data={(query) =>
-                     new Promise((resolve, reject) => {
-                        let productData = [];
-                        axios({
-                           method: "GET",
-                           url: "/admin/products",
-                           headers: {
-                              userid: localStorage.getItem("userId"),
+            <Grid item xs={12}>
+               {
+                  <MaterialTable
+                     title="Products List"
+                     options={{
+                        search: false,
+                        pageSize: 10,
+                        pageSizeOptions: [5, 10, 15, 20, 25, 30],
+                     }}
+                     tableRef={tableRef}
+                     columns={tableColumns}
+                     actions={[
+                        {
+                           icon: "edit",
+                           tooltip: "edit product",
+                           onClick: (event, rowData) => {
+                              editProductHandler(rowData._id);
                            },
-                        }).then((response) => {
-                           productData = [..._get(response, "data", [])];
-                           resolve({
-                              data: productData,
-                              page: _get(query, "page", 1),
-                              totalCount: productData.length,
+                        },
+                        {
+                           icon: "delete",
+                           tooltip: "delete product",
+                           onClick: (event, rowData) => {
+                              const { deleteProduct } = props;
+                              deleteProduct(rowData?._id ?? "", ({ isloading, success, error }) => {
+                                 if (success) {
+                                    tableRef.current && tableRef.current.onQueryChange();
+                                 }
+                              });
+                           },
+                        },
+                        {
+                           icon: "refresh",
+                           tooltip: "Refresh Products",
+                           isFreeAction: true,
+                           onClick: () => tableRef.current && tableRef.current.onQueryChange(),
+                        },
+                     ]}
+                     data={(query) =>
+                        new Promise((resolve, reject) => {
+                           let productData = [];
+                           axios({
+                              method: "GET",
+                              url: "/admin/products",
+                              headers: {
+                                 userid: localStorage.getItem("userId"),
+                              },
+                           }).then((response) => {
+                              productData = [..._get(response, "data", [])];
+                              resolve({
+                                 data: productData,
+                                 page: _get(query, "page", 1),
+                                 totalCount: (productData || []).length,
+                              });
                            });
-                        });
-                     })
-                  }
-               />
-            }
+                        })
+                     }
+                  />
+               }
             </Grid>
          </Grid>
-      );
-   }
+      </div>
+   );
+};
 
 export default connect(null, { deleteProduct, fetchSingleProduct })(ProductList);
